9639c203eb7a1d2083e9bdadfc77d72ef521b7ce
bikaxhoda <opu6612@gmail.com>
2025-07-16 16:43:37 +0600

    i am not able

---
+++ js/log-in.js
@@ -0,0 +1,10 @@
+
+// step 1: Add an event listener to the login button
+document.getElementById(`btn-login`).addEventListener(`click`, function(event) {
+    // step 2: prevent the default action of the button
+    event.preventDefault();
+    // step 3: get the phone number and pin from the input fields
+    const phoneNumber = document.getElementById(`phone-number`);
+    console.log(`Phone Number`);
+}
+); 
