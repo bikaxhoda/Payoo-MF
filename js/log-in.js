ffb0d04763719f9c3895fd11c9de6c7e56f7f074
bikaxhoda <opu6612@gmail.com>
2025-07-18 15:55:58 +0600

    second

--- js/log-in.js
+++ js/log-in.js
@@ -1,10 +1,14 @@
 
 // step 1: Add an event listener to the login button
 document.getElementById(`btn-login`).addEventListener(`click`, function(event) {
-    // step 2: prevent the default action of the button
+ // step 2: prevent the default action of the button
     event.preventDefault();
-    // step 3: get the phone number and pin from the input fields
-    const phoneNumber = document.getElementById(`phone-number`);
-    console.log(`Phone Number`);
-}
-); 
+
+// step 3: get the phone number and pin from the input fields
+    const phoneNumber = document.getElementById(`phone-number`).value;
+    const pinNumber = document.getElementById(`pin-number`).value;
+    
+    console.log(PhoneNumber, pinNumber );
+
+    
+}); 
